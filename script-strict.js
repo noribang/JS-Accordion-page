758f005991903f415b5e2d0d19d2819ae31e78a7
noribang <hoonk9387@gmail.com>
2018-01-28 21:47:19 -0600

    Created slightly different accordion page with different styling and javascript.

--- script-strict.js
+++ script-strict.js
@@ -1,61 +1,15 @@
-"use strict"
-// # 1. Will open clicking on link but will remain open 
-//      if another link is clicked.
-// Listen for clicks in the browser
-
-// document.addEventListener('click', function (event) {
-// 	// Only run if it has an accordian toggle
-// 	if (!event.target.classList.contains('accordion-toggle')) return;
-
-// 	// Get content area by hash.
-// 	let content = document.querySelector(event.target.hash);
-// 	// If no content area hash found then quit (return undefined).
-// 	if (!content) return;
-// 	console.log(content);
-
-// 	// Show/ hide content. Hash add/ remove class='active'.
-// 	content.classList.toggle('active');
-
-// 	// Prevent default link behaviour.
-// 	event.preventDefault();
-
-// }, false);
-
-
-// #2
-// Refactored to allow only one link to open at a time. 
-// True accordion functionality.
-// Listen for clicks in the browser
-document.addEventListener('click', function (event) {
-	// Only run if it has an accordian toggle
-	if (!event.target.classList.contains('accordion-toggle')) return;
-
-	// Get content area by hash.
-	let content = document.querySelector(event.target.hash);
-	// If no content area hash found then quit (return undefined).
-	if (!content) return;
-	console.log(content);
-
-	// Prevent default link behaviour.
-	event.preventDefault();
-	console.log("preventDefault()");
-	console.log("Stops here");
-	// Check if our content area is already open.
-	// a. If .active exists then remove it.
-	if ( content.classList.contains('active') ) {
-			content.classList.remove('active');
-			return;
-	}
-	console.log("if...remove()");
-
-
-	// Get all .accordion.
-	let accordions = document.querySelectorAll('.accordion');
-	console.log("accordions: ", accordions);
-	for (let i = 0; i < accordions.length; i++) {
-		accordions[i].classList.remove('active');
-	}
-	console.log("Add .active");
-	content.classList.add('active');
-
-}, false); 
+var acc = document.getElementsByClassName("accordion");
+var i;
+console.log(acc);
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		}
+		else {
+			panel.style.display = "block";
+		}
+	});
+}
