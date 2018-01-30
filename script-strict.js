607b8b997772845ce495737ffc117862c730d9d7
noribang <hoonk9387@gmail.com>
2018-01-29 23:02:20 -0600

    Added JS and styling to animate accordion page and included unicode +/- to display when each section opens and closes.

--- script-strict.js
+++ script-strict.js
@@ -1,15 +1,27 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 console.log(acc);
+
 for (i = 0; i < acc.length; i++) {
 	acc[i].addEventListener("click", function() {
+		// Toggle .active for button element w/ .accordion.
 		this.classList.toggle("active");
 		var panel = this.nextElementSibling;
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
+		// 1. Non-Animated code for div .panel
+		// if (panel.style.display === "block") {
+		// 	panel.style.display = "none";
+		// }
+		// else {
+		// 	panel.style.display = "block";
+		// }
+
+		// 2. Animated code for div .panel.
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
 		}
 		else {
-			panel.style.display = "block";
+			panel.style.maxHeight = panel.scrollHeight + "px";
 		}
+		console.log("panel.scrollHeight: ", panel.scrollHeight);
 	});
 }
